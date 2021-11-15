48a88bdfda47d0b7baf74813d39e890be920fe39
Chaitanya Shashwat <50068540+ChaitanyaShashwat@users.noreply.github.com>
2021-11-15 15:32:55 +0530

    Update Solution 4.23.cpp

--- Solution 4.23.cpp
+++ Solution 4.23.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <iostream.h>
+#include <stdlib.h>
 #include <pthread.h>
  
 #define MAX_THREADS 2
@@ -8,12 +10,15 @@ std::vector<int> primes;
 void *printprime(void *ptr, int N)
 {
     int j, flag;
-    int i = (int)(long long int)ptr;//explicit conversion due to pointer being void
+    int i = (int)ptr;//explicit conversion due to pointer being void
     while (i < N)
     {
-        
+        primes[i]=0;//initializing to remove junk values
+     
         cout<<"Thread ID"<<pthread_self()<<"checking"<<i<<endl;
+     
         flag = 0;
+     
         for (j = 2; j <= i / 2; j++)
         {
             if (i % j == 0)
@@ -26,8 +31,8 @@ void *printprime(void *ptr, int N)
 
         if (flag == 0 && (i > 1))
         {
-            primes.emplace_back(1);
-            //if prime
+            primes[i]=1;
+            //if prime, it'll store 1(true) at index 
         }
         i += MAX_THREADS;
         //so that threads work one after the other or simply to reduce redundancy
@@ -37,7 +42,12 @@ void *printprime(void *ptr, int N)
 int main()
 {
     pthread_t tid[MAX_THREADS] = {{0}};
-    int count = 0;
+    int count = 0,N;
+    
+    cout<<"Enter N(number till which primes have to be computed)";
+    cin>>N;
+    cout<<endl;
+ 
     //THREAD CREATION along with passing the printprime function
     for (count = 0; count < MAX_THREADS; count++)
     {
@@ -53,7 +63,6 @@ int main()
         pthread_join(tid[count], NULL);
     }
 
-    int c = 0;
     for (count = 0; count < N; count++)
         if (primes[count] == 1)
         {
